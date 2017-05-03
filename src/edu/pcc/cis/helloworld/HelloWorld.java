80fc533d54047429e06db1572ebc89f957ba3493
Colby Muchmore <colby.muchmore@pcc.edu>
2017-05-02 22:08:47 -0700

    added a main method that prints Hello, World!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -5,4 +5,7 @@ package edu.pcc.cis.helloworld;
  * @version 5/2/2017
  */
 public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello, World!");
+    }
 }
